b2c4e02a42bfefe379af98b029b8dbc459a4961f
sahajathoorpu <sthoorpu@rivier.edu>
2025-05-01 16:09:49 -0400

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from website import create_app
-from website.models import init_db
-
-app = create_app()
-
-if __name__ == '__main__':
-    init_db()
-    app.run(debug=True)
